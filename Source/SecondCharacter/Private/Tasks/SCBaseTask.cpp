b6fb0806d49d24098c80c6cb20c5d48d9a803ebb
Madmer <mr.madmer@gmail.com>
2024-11-03 10:42:23 +0700

    Storage game finished. Added tasks logic, bug fixes

---
+++ Source/SecondCharacter/Private/Tasks/SCBaseTask.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SecondCharacter/Public/Tasks/SCBaseTask.h"
+
+#include "Kismet/GameplayStatics.h"
+
+FText USCBaseTask::GetDescription_Implementation()
+{
+	return FText::FromString(TEXT("Base task description."));
+}
+
+AActor* USCBaseTask::GetActorOfClass(const TSubclassOf<AActor> Class)
+{
+	return UGameplayStatics::GetActorOfClass(this, Class);
+}
+
+TArray<AActor*> USCBaseTask::GetAllActorsOfClass(const TSubclassOf<AActor> Class)
+{
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsOfClass(this, Class, Actors);
+
+	return Actors;
+}
+
+TArray<AActor*> USCBaseTask::GetAllActorsWithTag(const FName Tag)
+{
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsWithTag(this, Tag, Actors);
+
+	return Actors;
+}
---
+++ Source/SecondCharacter/Public/Tasks/SCBaseTask.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SCBaseTask.generated.h"
+
+class USCBaseTask;
+
+USTRUCT(BlueprintType)
+struct FTaskInfo
+{
+	GENERATED_BODY()
+
+	/** Tablet task info.*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText ShortTaskDescription;
+
+	/** Billboard task info.*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText TaskDescription;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnTaskFinished);
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class SECONDCHARACTER_API USCBaseTask : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnTaskFinished OnTaskFinished;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintPure)
+	FText GetDescription();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void StartTask();
+
+protected:
+	UFUNCTION(BlueprintCallable, meta=(DeterminesOutputType="Class"))
+	AActor* GetActorOfClass(const TSubclassOf<AActor> Class);
+
+	UFUNCTION(BlueprintCallable, meta=(DeterminesOutputType="Class"))
+	TArray<AActor*> GetAllActorsOfClass(const TSubclassOf<AActor> Class);
+
+	UFUNCTION(BlueprintCallable)
+	TArray<AActor*> GetAllActorsWithTag(const FName Tag);
+};
